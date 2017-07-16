ec9425cee82db3c839b566919d38a0a7c23d6fa9
kguo901 <guo.901@osu.edu>
2017-07-16 12:05:36 -0400

    added links to root dropdown menus

---
+++ js/navigation.js
@@ -0,0 +1,8 @@
+$(function() {
+    var elements = document.getElementsByClassName("dropdown-toggle");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', function(){
+            window.location.href = this.getAttribute("href");
+        });
+    }
+});
