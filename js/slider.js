5bfbc3ee0894d5f87a4e77faa62c14e400b5be59
kguo901 <guo.901@osu.edu>
2018-03-25 13:19:36 -0400

    updated upcomming ements

--- js/slider.js
+++ js/slider.js
@@ -78,9 +78,9 @@ function currentSlide(num){
 var imgs = ['img/header/cosi.jpg', 'img/header/brain.jpg',  'img/header/fmri.jpg', 'img/header/graduate.jpg'];
 var ribbonLinks = ['', 'events.html', 'index.html#recent-events', 'index.html#recent-events'];
 var ribbons = ['', 'img/ribbons/upcoming_events.png', 'img/ribbons/recent_news.png', 'img/ribbons/recent_news.png'];
-var header = ['Welcome to SLAM Lab!', 'Traumatic Brain Injury Summit', "New Members","Special Announcement"];
+var header = ['Welcome to SLAM Lab!', 'Brain Health & Performance Summit', "New Members","Special Announcement"];
 var text = ['We are the auditory neuroscience lab at the Ohio State University\'s Department of Speech & Hearing Science. We study the connection between speech, language, and music in the brain.',
-'March 14 - Yune Lee will be presenting his work on traumatic brain injury at the Brain Injury Association of Ohio’s 2018 TBI Summit, sponsored by CBI. <a href="https://www.biausa.org/find-bia/states/events/2018-ohio-brain-injury-summit"><i class="fa fa-link"></i> MORE</a>',
+'April 5 - Dr. Yune S. Lee will be presenting Ohio State Lightning Talks at the Brain Health and Performance Summit from 1-1:50 pm. <a href="https://wexnermedical.osu.edu/neurological-institute/brain-health-and-performance-summit/agenda"><i class="fa fa-link"></i> MORE</a>',
 'SLAM Lab welcomes new members, Kristen McCormack and Parris Miller!',
 'Congratualtions to SLAMERS Matt Heard and Geoff Green for gaining acceptace to Graduate programs at OSU!'
 ];
