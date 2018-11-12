69397e28ac0806f916661c8627b41acd7f574f66
kguo901 <guo.901@osu.edu>
2018-11-12 10:26:40 -0500

    event and people updates

--- js/slider.js
+++ js/slider.js
@@ -78,11 +78,11 @@ function currentSlide(num){
 var imgs = ['img/header/fmri.jpg', 'img/header/newmember.jpg', 'img/header/party.jpg', 'img/header/welcome1.jpg'];
 var ribbonLinks = ['', 'index.html#recent-events', 'index.html#recent-events', 'events.html'];
 var ribbons = ['', 'img/ribbons/recent_news.png', 'img/ribbons/recent_news.png', 'img/ribbons/upcoming_events.png'];
-var header = ['Welcome to SLAM Lab!', "New Members","Special Announcement", "Dance Dance Revolution Therapy"];
+var header = ['Welcome to SLAM Lab!', "New Members","Special Announcement", "MASCATS Talk"];
 var text = ['We are the auditory neuroscience lab at the Ohio State University\'s Department of Speech & Hearing Science. We study the connection between speech, language, and music in the brain.',
 'SLAM Lab welcomes new members Emily Cushing and Marley Musara!',
 'Matt\'s rhythm & grammar neuroimaging study is featured in the NSF press release! Good Job!',
-'SLAM Lab will be hosting DDR therapy sessions starting Monday, November 11, at Martha Morehouse.'
+'Dr. Lee will be speaking at the Music and Science Colloquium and Teaching Series (MASCATS) on Nov 26.'
 ];
 
 var j = 0;
